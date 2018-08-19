934ce51b939871733af8be723e004dc92aa556b6
Magnus Lator <magosla2001@gmail.com>
2018-08-19 01:32:18 +0100

    removed API key

--- app/src/main/java/com/naijaplanet/magosla/android/moviesplanet/Config.java
+++ app/src/main/java/com/naijaplanet/magosla/android/moviesplanet/Config.java
@@ -1,7 +1,7 @@
 package com.naijaplanet.magosla.android.moviesplanet;
 
 public class Config {
-    public static final String MOVIEDB_API_KEY ="0d116ce78605dec3aac220965579bb2b";
+    public static final String MOVIEDB_API_KEY ="XXXXXXXXXXX";  // themoviedb.org Developer's API key
     public static final String MOVIEDB_THUMBNAIL_SIZE = "w185";
     public static final String MOVIEDB_FULL_SIZE = "w500";
     public static final String MOVIEDB_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
